03c370abcf96436b2915edf5ca01cd333663149f
Paulo Henrique Fonseca <paulofonseca.h@gmail.com>
2020-06-21 17:08:16 -0300

    Início do Projeto
    Subi os arquivos da minha maquina para o repositório

---
+++ src/Armas.java
@@ -0,0 +1,32 @@
+public class Armas {
+	
+	private int alcance;
+	private int bonus;
+	private boolean ehdestruida;
+	
+	public Armas(int a, int b, boolean d) {
+		alcance = a;
+		bonus = b;
+		ehdestruida = d;
+	}	
+	
+	public boolean isEhdestruida() {
+		return ehdestruida;
+	}
+	public void setEhdestruida(boolean ehdestruida) {
+		this.ehdestruida = ehdestruida;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	public int getAlcance() {
+		return alcance;
+	}
+	public void setAlcance(int alcance) {
+		this.alcance = alcance;
+	}	
+	
+}
